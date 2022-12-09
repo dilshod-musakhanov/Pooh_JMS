53ea93c124c6148bb03d914b2ba811f68b9c412e
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-12-09 13:36:23 +0400

    Pooh JMS(updated)

--- src/main/java/ru/job4j/pooh/QueueService.java
+++ src/main/java/ru/job4j/pooh/QueueService.java
@@ -1,9 +1,27 @@
 package ru.job4j.pooh;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class QueueService implements Service {
+    private final Map<String, ConcurrentLinkedQueue<String>> queue = new ConcurrentHashMap<>();
 
     @Override
     public Resp process(Req req) {
-        return null;
+        String text = "";
+        String status = "200";
+        if ("POST".equals(req.httpRequestType())) {
+            queue.putIfAbsent(req.getSourceName(), new ConcurrentLinkedQueue<>());
+            queue.get(req.getSourceName()).add(req.getParam());
+        } else if ("GET".equals(req.httpRequestType())) {
+            text = queue.getOrDefault(
+                    req.getSourceName(), new ConcurrentLinkedQueue<>()).poll();
+        }
+        if (text == null) {
+            text = "";
+            status = "204";
+        }
+        return new Resp(text, status);
     }
 }
--- src/main/java/ru/job4j/pooh/TopicService.java
+++ src/main/java/ru/job4j/pooh/TopicService.java
@@ -1,9 +1,29 @@
 package ru.job4j.pooh;
 
-public class TopicService implements Service {
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
+public class TopicService implements Service {
+    private final ConcurrentHashMap<String, ConcurrentHashMap<String, ConcurrentLinkedQueue<String>>> topics
+            = new ConcurrentHashMap<>();
    @Override
     public Resp process(Req req) {
-        return null;
+       String text = "";
+       String status = "200";
+       if ("POST".equals(req.httpRequestType())) {
+           for (ConcurrentLinkedQueue<String> queue : topics.getOrDefault(
+                   req.getSourceName(), new ConcurrentHashMap<>()).values()) {
+               queue.add(req.getParam());
+           }
+       } else if ("GET".equals(req.httpRequestType())) {
+           topics.putIfAbsent(req.getSourceName(), new ConcurrentHashMap<>());
+           topics.get(req.getSourceName()).putIfAbsent(req.getParam(), new ConcurrentLinkedQueue<>());
+           text = topics.get(req.getSourceName()).get(req.getParam()).poll();
+       }
+       if (text == null) {
+           text = "";
+           status = "204";
+       }
+       return new Resp(text, status);
     }
 }
